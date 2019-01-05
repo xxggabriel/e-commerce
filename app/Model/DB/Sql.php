a375a45200bed9f113c2f81b11769dc803304b84
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-01-05 02:38:56 -0200

    Class Sql adicionada

---
+++ app/Model/DB/Sql.php
@@ -0,0 +1,67 @@
+<?php 
+
+namespace App\Model\DB;
+
+use Config\FileConfig;
+
+class Sql
+{
+
+    private $conn;
+    public function __construct()
+    {
+		header("Content-Type: text/html; charset=UTF-8",true);
+        try {
+            $config = new FileConfig();
+            $this->conn = new \PDO('mysql:host='.$config->getDatabase('HOST').
+            ';dbname='.$config->getDatabase('DB_NAME').';'.
+            'port='. $config->getDatabase('PORT'),
+            $config->getDatabase('USER'),
+			$config->getDatabase('PASSWORD'),
+			array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES ".$config->getDatabase('CHARSET')));   
+        } catch (\PDOException $e) {
+            throw new \Exception("Erro: ".$e->getMessage()."\n".
+                                "Code: ".$e->getCode()."\n".
+                                "File: ".$e->getFile()."\n".
+                                "Line: ".$e->getLine()."\n" );
+        }
+        
+    }
+
+    private function setParams($statement, $parameters = array())
+	{
+		foreach ($parameters as $key => $value) {
+			
+			$this->bindParam($statement, $key, $value);
+		}
+	}
+	private function bindParam($statement, $key, $value)
+	{
+		$statement->bindParam($key, $value);
+	}
+	public function query($rawQuery, $params = array())
+	{
+		$stmt = $this->conn->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+	}
+	public function select($rawQuery, $params = array()):array
+	{
+		$stmt = $this->conn->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function backup()
+	{
+		$config = new Config;
+		exec("mysqldump --routines -u ".$config->getDatabase("USER")." -p". $config->getDatabase("PASSWORD")." ".$config->getDatabase("DB_NAME") ."> ". $_SERVER["DOCUMENT_ROOT"] . "/app/Model/DB/Backups/backup-".date("Y-m-d_H:i:s").".sql");
+		
+		// sleep(5);
+		exec("git add ".$_SERVER["DOCUMENT_ROOT"] . "/app/Models/DB/Backups/*");
+		exec("git commit -m 'Backup do banco de dados '". date("Y/m/d"));
+		exec("git push origin master");
+		
+	}	
+}
