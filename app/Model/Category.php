fdec58832fc96c5a1d6b06e900338d3329df3b2e
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2019-01-06 16:08:40 -0200

    CRUD criado

---
+++ app/Model/Category.php
@@ -0,0 +1,73 @@
+<?php 
+
+namespace App\Model;
+
+use App\Model\DB\Sql;
+
+class Category
+{
+
+    private $name;
+
+    public function create($name)
+    {
+        $this->setname($name);
+        $sql = new Sql();
+
+        $sql->query("CALL create_category(:name)", [
+            ":name" => $this->getName()
+        ]);
+    }
+
+    public function read($id_category = null)
+    {
+        $sql = new Sql();
+
+
+        return $sql->select("CALL read_category(:id_category)",[
+            ":id_category" => $id_category
+        ]);
+    }
+
+    public function update($id_category, $data)
+    {
+        $sql = new Sql();
+
+        foreach($data as $key => $value){
+
+            $sql->query("UPDATE tb_category SET $key = :value WHERE id_category = :id_category",[
+                ":value" => $value,
+                ":id_category" => $id_category
+            ]);
+        }
+    }
+
+    public function delete($id_category)
+    {
+        $sql = new Sql();
+
+        $sql->query("CALL delete_category(:id_category)",[
+            ":id_category" => $id_category
+        ]);
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
